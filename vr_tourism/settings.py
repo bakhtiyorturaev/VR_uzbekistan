92c6589d6aa5858c05707e394d03cb556ec20e81
baxtiyor <baxti.dev1@gmail.com>
2025-05-08 00:13:53 +0500

    ..

--- vr_tourism/settings.py
+++ vr_tourism/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = [baxtiyor1.pythonanywhere.com]
+ALLOWED_HOSTS = ['baxtiyor1.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'jazzmin',
